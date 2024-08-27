81d6873da3552e3acd43380b4ec2cdfbfd21858f
Onggo Wahyudi <carnegzs@gmail.com>
2024-08-27 17:22:13 +0800

    create a script to automate Topci creation

---
+++ kafka-topics.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+
+# Docker Container Name
+CONTAINER_NAME="broker"
+
+# Kafka broker address
+BROKER_ADDRESS="localhost:9092"
+
+# Topic name
+TOPICS=(
+    "UserInfoCreated"
+    "UserInfoDeleted"
+    "GroupInfoCreated"
+    "GroupInfoDeleted"
+    "TaskInfoCreated"
+    "TaskInfoDeleted"
+)
+
+# Number of partitions
+PARTITIONS=2
+
+# Replication factor
+REPLICATION_FACTOR=1
+
+docker exec -it br
+
+# Create topic
+
+for TOPIC in "${TOPICS[@]}"; do
+    docker exec $CONTAINER_NAME /bin/sh -c "
+        kafka-topics --create \
+            --bootstrap-server $BROKER_ADDRESS \
+            --replication-factor $REPLICATION_FACTOR \
+            --partitions $PARTITIONS \
+            --topic $TOPIC 
+    "
+
+done
+
+
